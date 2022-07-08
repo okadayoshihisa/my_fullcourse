d86ff8b60142ff5b95d3e4524fa1952e46509372
0103494705 <sayasaya714417@gmail.com>
2022-07-08 15:16:48 +0900

    Add: 生成したフルコース画像を保存するfullcourse_imageカラムをUsersテーブルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_01_065046) do
+ActiveRecord::Schema.define(version: 2022_07_08_060014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_07_01_065046) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "fullcourse_image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
