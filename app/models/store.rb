47e669a688e89eb73d933ec5bb223114525b5823
0103494705 <sayasaya714417@gmail.com>
2022-08-04 20:26:45 +0900

    Add: ストアテーブルの住所を登録するときに自動で緯度経度が登録されるようモデルにgeocoded_by :addressを追記

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,6 @@
 class Store < ApplicationRecord
   has_many :fullcourse_menus
+
+  geocoded_by :address # addressカラムにジオコーディングを実装
+  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? } # addressカラムが変更(保存や更新)されたらジオコーディングが行われる
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_17_154010) do
+ActiveRecord::Schema.define(version: 2022_08_04_094418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2022_07_17_154010) do
     t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
