690157426395af8a7b7827c68c6e309b68b459ef
0103494705 <sayasaya714417@gmail.com>
2022-08-16 14:38:39 +0900

    Fix: twitter認証をキャンセルした時にログインページに飛ぶように修正

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,37 +1,31 @@
 # app/controllers/oauths_controller.rb
 class OauthsController < ApplicationController
-  skip_before_action :require_login, raise: false
-      
-  # sends the user on a trip to the provider,
-  # and after authorizing there back to the callback url.
+  skip_before_action :require_login
+
   def oauth
     login_at(params[:provider])
   end
       
   def callback
     provider = params[:provider]
-    if @user = login_from(provider)
-      redirect_to fullcourses_path, success: "#{provider.titleize}でログインしました"
-    else
-      begin
-        @user = create_from(provider)
-        # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
-
-        reset_session # protect from session fixation attack
-        auto_login(@user)
-        redirect_to fullcourses_path, success: "#{provider.titleize}でログインしました"
-      rescue
-        redirect_to login_path, danger: "#{provider.titleize}でのログインに失敗しました"
-      end
+    debugger
+    if auth_params[:denied].present?
+      redirect_to login_path, danger: t('defaults.message.cancel', item: t('defaults.login'))
+      return
     end
+    create_user_from(provider) unless (@user = login_from(provider))
+    redirect_to fullcourses_path, success: t('user_sessions.create.success')
   end
-  
-  #example for Rails 4: add private method below and use "auth_params[:provider]" in place of 
-  #"params[:provider] above.
 
   private
 
   def auth_params
-    params.permit(:code, :provider)
+    params.permit(:code, :provider, :denied)
+  end
+
+  def create_user_from(provider)
+    @user = create_from(provider)
+    reset_session
+    auto_login(@user)
   end
 end
