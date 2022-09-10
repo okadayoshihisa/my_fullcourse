642406ada53eb1f0ef820a001fa6ab616eaaad4a
0103494705 <sayasaya714417@gmail.com>
2022-09-10 13:53:51 +0900

    Fix: パスワードリセットに関するページのデザインを修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,14 +11,14 @@ class PasswordResetsController < ApplicationController
 
   def edit
     @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
+    @user = User.load_from_reset_password_token(@token)
 
     not_authenticated if @user.blank?
   end
 
   def update
     @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
+    @user = User.load_from_reset_password_token(@token)
 
     return not_authenticated if @user.blank?
 
