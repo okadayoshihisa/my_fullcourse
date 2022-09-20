7c8f725d800fb17b3b6f64c09851ee15895d21e7
0103494705 <sayasaya714417@gmail.com>
2022-09-21 00:55:49 +0900

    Add: storeモデルのバリデーションテスト

---
+++ spec/models/store_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Store, type: :model do
+  it '店名、住所、緯度、軽度、TELの組み合わせが重複している時登録できないこと' do
+    store = create(:store)
+    duplicated_store = build(:store, name: store.name)
+    expect(duplicated_store).to be_invalid
+    expect(duplicated_store.errors[:name]).to eq ['はすでに存在します']
+  end
+
+  it '店名、住所、緯度、軽度、TELの組み合わせが異なる時登録できること' do
+    store = create(:store)
+    another_store = build(:store)
+    expect(store).to be_valid
+  end 
+end
