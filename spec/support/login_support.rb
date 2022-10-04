eab4b09dfa4e7d00439b990995eb353732b0afe8
0103494705 <sayasaya714417@gmail.com>
2022-10-04 22:53:49 +0900

    Add: ログイン処理をモジュールで共通化

---
+++ spec/support/login_support.rb
@@ -0,0 +1,9 @@
+module LoginSupport
+  def login(user)
+    visit root_path
+    click_link('ログイン')
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
+  end
+end
