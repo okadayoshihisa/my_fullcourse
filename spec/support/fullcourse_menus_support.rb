39c593fa7d5d23951353d5f994be8962d0938f66
0103494705 <sayasaya714417@gmail.com>
2022-10-06 06:09:05 +0900

    Add: フルコースメニューに関するテストを追加

--- spec/support/fullcourse_menus_support.rb
+++ spec/support/fullcourse_menus_support.rb
@@ -27,4 +27,33 @@ module FullcourseMenusSupport
     fill_in 'name_7', with: 'tokyo'
     click_button '登録'
   end
+
+  def update_menus(user)
+    visit edit_fullcourse_menu_path(user)
+    genre = page.all('div.btn.genre-selecter')
+    fill_in 'オードブル', with: 'update_zensai'
+    fill_in 'name_0', with: 'osaka'
+    genre[1].click
+    fill_in 'スープ', with: 'update_soup'
+    fill_in 'name_1', with: 'osaka'
+    genre[2].click
+    fill_in '魚料理', with: 'update_fish'
+    fill_in 'name_2', with: 'osaka'
+    genre[3].click
+    fill_in '肉料理', with: 'update_meat'
+    fill_in 'name_3', with: 'osaka'
+    genre[4].click
+    fill_in '主菜(メイン)', with: 'update_main'
+    fill_in 'name_4', with: 'osaka'
+    genre[5].click
+    fill_in 'サラダ', with: 'update_salad'
+    fill_in 'name_5', with: 'osaka'
+    genre[6].click
+    fill_in 'デザート', with: 'update_desert'
+    fill_in 'name_6', with: 'osaka'
+    genre[7].click
+    fill_in 'ドリンク', with: 'update_drink'
+    fill_in 'name_7', with: 'osaka'
+    click_button '登録'
+  end
 end
