f8a29f3311a1f71abc1c9e02432a1a96a3e82774
0103494705 <sayasaya714417@gmail.com>
2022-10-12 05:05:46 +0900

    Fix: メーラーでgmailを利用するように修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      enable_starttls_auto: true,
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            ENV['GMAIL_ADDRESS'], # 自分のGmailアドレス
+      password:             ENV['GMAIL_PASSWORD'], # Gmailで設定したアプリパスワード
+      authentication:       :plain
+  }
+  config.action_mailer.default_url_options = { host: 'https://my-fullcourse.com/' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
