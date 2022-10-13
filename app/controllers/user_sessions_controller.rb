1a16283b5677bda792f241c1a09ea48e439c86dc
0103494705 <sayasaya714417@gmail.com>
2022-10-13 17:56:58 +0900

    Add: ゲストログインのルーティングと処理を追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,5 +1,5 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login, only: %i[new create]
+  skip_before_action :require_login, only: %i[new create guest_login]
 
   def new; end
 
@@ -16,5 +16,20 @@ class UserSessionsController < ApplicationController
   def destroy
     logout
     redirect_to login_path, success: t('.success')
+  end
+
+  def guest_login
+    if current_user
+      redirect_to fullcourses_path, alert: t('.fail')
+    else
+      random_value = SecureRandom.hex
+      user = User.create!(name: 'ゲストユーザー',
+                          email: "#{random_value}@example.com",
+                          password: random_value,
+                          password_confirmation: random_value,
+                          guest: true)
+      auto_login(user)
+      redirect_to fullcourses_path, success: t('.success')
+    end
   end
 end
